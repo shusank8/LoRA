3c2fa86d081b1edd74c546bbf3e712cbedb9d50b
shusank8 <shusank8basyal@gmail.com>
2025-07-21 14:33:42 -0400

    init variables

--- src/lorabase.py
+++ src/lorabase.py
@@ -7,5 +7,11 @@ import torch.nn.functional as F
 
 class LoRABase:
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, rank = 8, lora_alpha = 8, lora_dropout = 0):
+        self.rank = rank
+        self.lora_alpha = lora_alpha
+        self.lora_dropout = nn.Dropout(lora_dropout)
+        self.scaling = self.lora_alpha / self.rank**(1/2)
+
+
+
